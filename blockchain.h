f716e19d8a4b54d9dbd26b40f57b8c3e1b71437a
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-11-08 19:06:26 +0100

    Update blockchain.h

--- blockchain.h
+++ blockchain.h
@@ -5,44 +5,55 @@
 #include <vector>
 #include <tuple>
 #include <ctime>
+#include <chrono>
 #include "block.h"
 #include "merkle.h"
+#include "storage.h"
 #include "..\..\lib\sha256_lib\picosha2.h"
 #include <boost/multiprecision/cpp_int.hpp>
 
 class Blockchain {
 private:
-	std::vector<Block> _blockchain;
+	std::vector<Block*> _blockchain;
 	std::map<std::string,std::string> _data;
 	std::string _merkleRoot;
 	std::time_t _timestamp;
 	boost::multiprecision::uint256_t _nonce;
 	std::string _hash;
+	std::time_t _deadline;
+	unsigned int _difficulty = 2;
+	const std::string _target = "0000000000000000000000000000000000000000000000000000000000000000";
+	Storage storage = Storage();
 public:
-	Blockchain(std::string blockZeroPrevHash) {
+	Blockchain(std::string blockZeroPrevHash, std::time_t deadline) {
 		std::cout << "Blockchain C'tor" << std::endl;
-		_blockchain.reserve(_blockchain.size() + 1);
-		_timestamp = std::time(nullptr);
+		_deadline = deadline;
+		_blockchain.reserve(_blockchain.size() + 1);	
 		unsigned int id = 0;
 		_merkleRoot = merkle::get_merkle_root_hash(_data);
+		_timestamp = std::time(nullptr);
 		std::string toHash = std::to_string(id) + "," + std::to_string(_timestamp) + "," + _merkleRoot + "," + blockZeroPrevHash;
 		validate_nonce(picosha2::hash256_hex_string(toHash));
-		_blockchain.push_back(Block(id, _timestamp, blockZeroPrevHash, _merkleRoot, _nonce, _hash, _data));
+		_blockchain.push_back(new Block(id, _timestamp, blockZeroPrevHash, _merkleRoot, _nonce, _hash, _data));
 	}
+
+	std::time_t getDeadline() { return _deadline; }
+
+	void setDifficulty(unsigned int newDifficulty) { _difficulty = newDifficulty; }
 
 	void hash() {
 		_merkleRoot = merkle::get_merkle_root_hash(_data);
-		std::string toHash = std::to_string(_blockchain.size()) + "," + std::to_string(_timestamp) + "," + _merkleRoot + "," + _blockchain.back().getHash();
+		std::string toHash = std::to_string(_blockchain.size()) + "," + std::to_string(_timestamp) + "," + _merkleRoot + "," + _blockchain.back()->getHash();
 		validate_nonce(picosha2::hash256_hex_string(toHash));
 	}
 
-	void validate_nonce(std::string const toHash) {
+	void validate_nonce(std::string const &toHash) {
 		boost::multiprecision::uint256_t nonce = 0;
 		std::string hash;
 		while (true) {
 			hash = toHash + nonce.str();
 			hash = picosha2::hash256_hex_string(hash);
-			if (hash.substr(0, 4) == "0000") break;
+			if (hash.substr(0, _difficulty) == _target.substr(0, _difficulty)) break;
 			nonce++;
 		}
 		_nonce = nonce;
@@ -52,16 +63,26 @@ public:
 	void addTx(std::string sender, std::string receiver) { _data.insert(std::pair<std::string, std::string>(sender, receiver));	}
 
 	void addBlock() {
+				
+		if (_blockchain.size() == 7) {
+			storage.appendBlock(_blockchain.at(0), ',');
+			_blockchain.erase(_blockchain.begin());
+		}
+
 		_blockchain.reserve(_blockchain.size() + 1);
 		_timestamp = std::time(nullptr);
 		hash();
-		_blockchain.push_back(Block(_blockchain.size(), _timestamp, _blockchain.back().getHash(), _merkleRoot, _nonce, _hash, _data));
+		_blockchain.push_back(new Block(_blockchain.back()->getId() + 1, _timestamp, _blockchain.back()->getHash(), _merkleRoot, _nonce, _hash, _data));
 		_data.clear();
 	}
 
-	void print() { for (auto el : _blockchain) { std::cout << "\n########################\n"<<std::endl; el.printBlock(); } }
+	void printBlockchain() { storage.printBlockchain(); }
+	void printLastBlock() { std::cout << "\n########################\n" << std::endl; _blockchain.back()->printBlock(); }
+		
 
-	~Blockchain() { std::cout << "Blockchain D'tor" << std::endl; }
+	void wipe() { for (auto el : _blockchain) el->~Block();	}
+
+	~Blockchain() { wipe(); std::cout << "Blockchain D'tor" << std::endl; }
 };
 
 #endif
