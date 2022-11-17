83d4e90a7409a889918b3541ad9042c043ec0481
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-11-17 16:56:07 +0100

    Update blockchain.h

--- blockchain.h
+++ blockchain.h
@@ -23,7 +23,8 @@ private:
 	std::time_t _deadline;
 	unsigned int _difficulty = 2;
 	const std::string _target = "0000000000000000000000000000000000000000000000000000000000000000";
-	Storage storage = Storage();
+	unsigned int _slidingWindowSize = 5;
+	Storage _storage = Storage();
 public:
 	Blockchain(std::string blockZeroPrevHash, std::time_t deadline) {
 		std::cout << "Blockchain C'tor" << std::endl;
@@ -38,8 +39,10 @@ public:
 	}
 
 	std::time_t getDeadline() { return _deadline; }
+	Storage* getStorage() { return &_storage; }
 
 	void setDifficulty(unsigned int newDifficulty) { _difficulty = newDifficulty; }
+	void setSlidingWindowSize(unsigned int newSize) { if(newSize > 0) _slidingWindowSize = newSize; }
 
 	void hash() {
 		_merkleRoot = merkle::get_merkle_root_hash(_data);
@@ -62,23 +65,36 @@ public:
 
 	void addTx(std::string sender, std::string receiver) { _data.insert(std::pair<std::string, std::string>(sender, receiver));	}
 
-	void addBlock() {
-				
-		if (_blockchain.size() == 7) {
-			storage.appendBlock(_blockchain.at(0), ',');
+	void slidingWindow() {
+		if (_blockchain.size() > _slidingWindowSize) {
+			_storage.appendBlock(_blockchain.at(0), ',');
+			_storage.createSingleBlockJson(_blockchain.at(0));
+			_blockchain.at(0)->~Block();
 			_blockchain.erase(_blockchain.begin());
 		}
+	}
 
+	void addBlock() {		
 		_blockchain.reserve(_blockchain.size() + 1);
 		_timestamp = std::time(nullptr);
 		hash();
 		_blockchain.push_back(new Block(_blockchain.back()->getId() + 1, _timestamp, _blockchain.back()->getHash(), _merkleRoot, _nonce, _hash, _data));
 		_data.clear();
+		slidingWindow();
 	}
 
-	void printBlockchain() { storage.printBlockchain(); }
+	void printBlockchain() { _storage.printBlockchain(); }
 	void printLastBlock() { std::cout << "\n########################\n" << std::endl; _blockchain.back()->printBlock(); }
 		
+	void addRemainingTxs() {
+		while (_blockchain.size() > 0) {
+			_storage.appendBlock(_blockchain.at(0), ',');
+			_storage.createSingleBlockJson(_blockchain.at(0));
+			_blockchain.at(0)->~Block();
+			_blockchain.erase(_blockchain.begin());
+		}
+		_storage.close();
+	}
 
 	void wipe() { for (auto el : _blockchain) el->~Block();	}
 
