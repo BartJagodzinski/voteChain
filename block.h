7e4222b70d35af58686e6c7ec0d08feb5f02852c
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-04 20:16:51 +0100

    Update block.h

--- block.h
+++ block.h
@@ -10,25 +10,25 @@
 
 class Block {
 private:
-// <header>
 	unsigned int _id;
 	std::time_t _timestamp;
 	std::string _prevHash;
 	std::string _merkleRoot;
-// </header>
 	boost::multiprecision::uint256_t _nonce;
 	std::string _hash;
 	std::map<std::string,std::string> _data;
 public:
 	Block(unsigned int id, std::time_t timestamp, std::string prevHash, std::string merkleRoot, boost::multiprecision::uint256_t nonce, std::string hash, std::map<std::string, std::string> &data)
-		: _id(id), _timestamp(timestamp), _prevHash(prevHash), _merkleRoot(merkleRoot), _nonce(nonce), _hash(hash), _data(data) {}
+		: _id(id), _timestamp(timestamp), _prevHash(prevHash), _merkleRoot(merkleRoot), _nonce(nonce), _hash(hash), _data(data) { std::cout << "Block C'tor" << std::endl;	}
 
-	unsigned int getId() { return _id; }
-	std::time_t getTimestamp() { return _timestamp; }
-	std::string getPrevHash() { return _prevHash; }
-	std::string getMerkleRoot() { return _merkleRoot; }
-	boost::multiprecision::uint256_t getNonce() { return _nonce; }
-	std::string	getHash() { return _hash; }
+	unsigned int getId() const { return _id; }
+	std::string getIdStr() const { return std::to_string(_id); }
+	std::time_t getTimestamp() const { return _timestamp; }
+	std::string getPrevHash() const { return _prevHash; }
+	std::string getMerkleRoot() const { return _merkleRoot; }
+	boost::multiprecision::uint256_t getNonce() const { return _nonce; }
+	std::string	getHash() const { return _hash; }
+	const std::map<std::string, std::string> &getData() { return _data; }
 
 	void printBlock() {
 		std::cout << "id         : " << _id << std::endl;
@@ -37,12 +37,14 @@ public:
 		std::cout << "merkleRoot : " << _merkleRoot << std::endl;
 		std::cout << "nonce      : " << _nonce << std::endl;
 		std::cout << "hash:      : " << _hash << std::endl;
-		for (auto &el : _data) {
-			std::cout << "------------------------------------------" << std::endl;
-			std::cout << "sender:  " << el.first << std::endl;
-			std::cout << "receiver: " << el.second << std::endl;
-		}
+		//for (auto &el : _data) {
+		//	std::cout << "------------------------------------------------------------------------------------" << std::endl;
+		//	std::cout << "sender:  " << el.first << std::endl;
+		//	std::cout << "receiver: " << el.second << std::endl;
+		//}
 	}
+
+	~Block() { std::cout << "Block D'tor" << std::endl; }
 };
 
 #endif
