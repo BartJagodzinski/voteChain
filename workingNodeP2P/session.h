5e796286b6f684d02a56edb9680ade67e6fb68e9
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-10 18:08:54 +0100

    Update session.h

--- workingNodeP2P/session.h
+++ workingNodeP2P/session.h
@@ -1,15 +1,10 @@
 #ifndef SESSION_H
 #define SESSION_H
-#include <cstdlib>
-#include <deque>
-#include <iostream>
-#include <list>
-#include <memory>
-#include <set>
-#include <utility>
 #include <boost/asio.hpp>
-#include "chat_message.hpp"
+#include <iostream>
+#include <deque>
 #include "peer.h"
+#include "chat_message.hpp"
 
 class Session : public Peer, public std::enable_shared_from_this<Session> {
 private:
@@ -18,47 +13,48 @@ private:
   chat_message _read_msg;
   std::deque<chat_message> _write_msgs;
 
-  void do_read_header() {
+  void _readHeader() {
     auto self(shared_from_this());
-    boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.data(), chat_message::header_length),
+      boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.data(), chat_message::header_length),
         [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-          if (!ec && _read_msg.decode_header()) do_read_body();
+          if (!ec && _read_msg.decode_header()) _readBody();
         });
   }
 
-  void do_read_body() {
+  void _readBody() {
     auto self(shared_from_this());
-    boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.body(), _read_msg.body_length()),
+      boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.body(), _read_msg.body_length()),
         [this, self](boost::system::error_code ec, std::size_t /*length*/) {
           if (!ec) {
-            //_room.deliver(_read_msg);
             std::cout << _read_msg.body() << std::endl;
-            do_read_header();
+            _readHeader();
           }
         });
   }
 
-  void do_write() {
+  void _write() {
     auto self(shared_from_this());
-    boost::asio::async_write(_socket, boost::asio::buffer(_write_msgs.front().data(), _write_msgs.front().length()),
+      boost::asio::async_write(_socket, boost::asio::buffer(_write_msgs.front().data(), _write_msgs.front().length()),
         [this, self](boost::system::error_code ec, std::size_t /*length*/) {
           if (!ec) {
             _write_msgs.pop_front();
-            if (!_write_msgs.empty()) do_write();
+            if (!_write_msgs.empty()) _write();
           }
         });
   }
 
 public:
-  Session(boost::asio::ip::tcp::socket socket) : _socket(std::move(socket)) {}
+  Session(boost::asio::ip::tcp::socket socket) : _socket(std::move(socket)) {  std::cout << "Session C'tor" << std::endl; }
 
-  void start() { do_read_header(); }
+  void start() { _readHeader(); }
 
   void deliver(const chat_message& msg) {
     bool write_in_progress = !_write_msgs.empty();
     _write_msgs.push_back(msg);
-    if (!write_in_progress) do_write();
+    if (!write_in_progress) _write();
   }
+
+  ~Session() { std::cout << "Session D'tor" << std::endl; }
 };
 
 #endif
