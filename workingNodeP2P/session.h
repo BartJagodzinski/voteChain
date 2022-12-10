94d03b4fe09d5077f7a2b5a5a061b6d25d4d9f77
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-10 15:43:30 +0100

    Update session.h

--- workingNodeP2P/session.h
+++ workingNodeP2P/session.h
@@ -10,12 +10,11 @@
 #include <boost/asio.hpp>
 #include "chat_message.hpp"
 #include "peer.h"
-#include "room.h"
 
 class Session : public Peer, public std::enable_shared_from_this<Session> {
 private:
   boost::asio::ip::tcp::socket _socket;
-  Room& _room;
+  std::shared_ptr<Peer> _peer;
   chat_message _read_msg;
   std::deque<chat_message> _write_msgs;
 
@@ -24,7 +23,6 @@ private:
     boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.data(), chat_message::header_length),
         [this, self](boost::system::error_code ec, std::size_t /*length*/) {
           if (!ec && _read_msg.decode_header()) do_read_body();
-          else _room.leave(shared_from_this());
         });
   }
 
@@ -33,10 +31,10 @@ private:
     boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.body(), _read_msg.body_length()),
         [this, self](boost::system::error_code ec, std::size_t /*length*/) {
           if (!ec) {
-            _room.deliver(_read_msg);
+            //_room.deliver(_read_msg);
+            std::cout << _read_msg.body() << std::endl;
             do_read_header();
           }
-          else _room.leave(shared_from_this());
         });
   }
 
@@ -48,17 +46,13 @@ private:
             _write_msgs.pop_front();
             if (!_write_msgs.empty()) do_write();
           }
-          else _room.leave(shared_from_this());
         });
   }
 
 public:
-  Session(boost::asio::ip::tcp::socket socket, Room& room) : _socket(std::move(socket)), _room(room) {}
+  Session(boost::asio::ip::tcp::socket socket) : _socket(std::move(socket)) {}
 
-  void start() {
-    _room.join(shared_from_this());
-    do_read_header();
-  }
+  void start() { do_read_header(); }
 
   void deliver(const chat_message& msg) {
     bool write_in_progress = !_write_msgs.empty();
