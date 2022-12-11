6c4bf973090cae19a8e66197ae97bedcc3543423
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-11 18:47:22 +0100

    Update mempool.h

--- mempool.h
+++ mempool.h
@@ -2,36 +2,42 @@
 #define MEMPOOL_H
 #include <iostream>
 #include <string>
-#include <vector>
-#include <tuple>
-#include <map>
-#include "..\..\lib\json_lib\json.hpp"
-#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/asio.hpp>
+#include "mempool_session.h"
 
 class Mempool {
 private:
-	std::map<std::string,std::string> _data;
+  	boost::asio::ip::tcp::acceptor _acceptor;
+	Room _room;
+	std::unordered_map<std::string, std::string> _data;
 	std::time_t _deadline;
-public:
-	Mempool(std::time_t deadline) : _deadline(deadline) { std::cout << "Mempool C'tor" << std::endl; }
+	bool _isOpen = true;
 
-	void getTxs(std::map<std::string, std::string> &candidateBlockData, unsigned int nTxToRemove) {
+	void _accept() {
+    	_acceptor.async_accept(
+        [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket) {
+          if (!ec) std::make_shared<MempoolSession>(std::move(socket), _room, _data)->start();
+          _accept();
+        });
+  	}
+
+public:
+	Mempool(boost::asio::io_context& io_context, const boost::asio::ip::tcp::endpoint& endpoint, std::time_t deadline) : _acceptor(io_context, endpoint), _deadline(deadline) { _accept(); std::cout << "Mempool C'tor" << std::endl; }
+
+	void closeMempool() { _isOpen = false; }
+
+	void getTxs(std::unordered_map<std::string, std::string> &candidateBlockData, unsigned int nTxToRemove) {
 		auto it = _data.begin();
+
 		// Both must be true to avoid removing more txs than are in mempool
 		while (it != _data.end() && nTxToRemove > 0) {
-			candidateBlockData.insert(std::pair<std::string, std::string>(it->first, it->second));
+			candidateBlockData.insert({it->first, it->second});
 			it = _data.erase(it);
 			--nTxToRemove;
 		}
 	}
 
-	void print() { for (auto el : _data) std::cout << el.first << ": " << el.second << std::endl; }
-
-	void addTx(std::string sender, std::string receiver) { _data.insert(std::pair<std::string, std::string>(sender, receiver)); }
-
 	~Mempool() { std::cout << "Mempool D'tor" << std::endl; }
 };
-
-
 
 #endif
