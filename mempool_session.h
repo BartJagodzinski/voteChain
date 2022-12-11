ce6d00af3770bd8a1539947de8f1befe0903d03e
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-11 18:48:25 +0100

    Create mempool_session.h

---
+++ mempool_session.h
@@ -0,0 +1,52 @@
+#ifndef MEMPOOL_SESSION_H
+#define MEMPOOL_SESSION_H
+#include <boost/asio.hpp>
+#include <unordered_map>
+#include <iostream>
+#include <deque>
+#include "message.hpp"
+#include "voter.h"
+#include "mempool_room.h"
+
+class MempoolSession : public Voter, public std::enable_shared_from_this<MempoolSession> {
+private:
+  std::unordered_map<std::string, std::string> &_data;
+  boost::asio::ip::tcp::socket _socket;
+  Room &_room;
+  Message _read_msg;
+
+  void _readHeader() {
+    auto self(shared_from_this());
+      boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.data(), Message::header_length),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec && _read_msg.decode_header()) _readBody();
+        });
+  }
+
+  void _readBody() {
+    auto self(shared_from_this());
+      boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.body(), _read_msg.body_length()),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec) {
+            std::string message = _read_msg.body();
+            if(message.size() == _read_msg.body_length()) {
+              _data.insert({message.substr(0, 64), message.substr(65, 128)});
+              for(auto el : _data) std::cout << el.first <<": " << el.second << std::endl;
+            }
+            _readHeader();
+          }
+        });
+  }
+
+public:
+  MempoolSession(boost::asio::ip::tcp::socket socket, Room& room, std::unordered_map<std::string, std::string> &data) : _socket(std::move(socket)), _room(room), _data(data) {  std::cout << "MempoolSession C'tor" << std::endl; }
+
+  void start() {
+    _room.join(shared_from_this());
+    _readHeader();
+    }
+
+  ~MempoolSession() { std::cout << "MempoolSession D'tor" << std::endl; }
+};
+
+#endif
