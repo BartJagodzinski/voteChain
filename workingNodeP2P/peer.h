ea70f59ca9979662c38af63439988b0597cf03e6
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-11 13:41:32 +0100

    Update peer.h

--- workingNodeP2P/peer.h
+++ workingNodeP2P/peer.h
@@ -1,11 +1,11 @@
 #ifndef PEER_H
 #define PEER_H
-#include "chat_message.hpp"
+#include "message.hpp"
 
 class Peer {
 public:
   virtual ~Peer() {}
-  virtual void deliver(const chat_message& msg) = 0;
+  virtual void deliver(const Message& msg) = 0;
 };
 
 #endif
