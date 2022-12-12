4dc538e8c8a55c074e0b5ec9bca338bcdc129833
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-12 20:20:29 +0100

    Update voter.h

--- src/voter.h
+++ src/voter.h
@@ -1,3 +1,6 @@
+// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Distributed under the Boost Software License, Version 1.0. http://www.boost.org/LICENSE_1_0.txt
+
 #ifndef VOTER_H
 #define VOTER_H
 #include "message.hpp"
