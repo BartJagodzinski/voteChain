cf91e07ce93544c432ebd3626d139a3055efd7b6
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-15 13:54:08 +0100

    Update mempool.h

--- src/mempool.h
+++ src/mempool.h
@@ -11,12 +11,13 @@ private:
 	MempoolRoom _room;
 	std::unordered_map<std::string, std::string> _data;
 	std::time_t _deadline;
-	bool _isOpen = true;
+
+	bool _isOpen() { return (std::time(nullptr) < _deadline) ? true : false; }
 
 	void _accept() {
     	_acceptor.async_accept(
         [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket) {
-          if (!ec) std::make_shared<MempoolSession>(std::move(socket), _room, _data)->start();
+          if (!ec && _isOpen()) std::make_shared<MempoolSession>(std::move(socket), _room, _data)->start();
           _accept();
         });
   	}
@@ -24,16 +25,13 @@ private:
 public:
 	Mempool(boost::asio::io_context& io_context, const boost::asio::ip::tcp::endpoint& endpoint, std::time_t deadline) : _acceptor(io_context, endpoint), _deadline(deadline) { _accept(); std::cout << "Mempool C'tor" << std::endl; }
 
-	void closeMempool() { _isOpen = false; }
-
-	void getTxs(std::unordered_map<std::string, std::string> &candidateBlockData, unsigned int nTxToRemove) {
+	void getVotes(std::unordered_map<std::string, std::string> &candidateBlockData, unsigned int nbOfVotes) {
 		auto it = _data.begin();
-
-		// Both must be true to avoid removing more txs than are in mempool
-		while (it != _data.end() && nTxToRemove > 0) {
+		// Both must be true to avoid removing more votes than are in mempool
+		while (it != _data.end() && nbOfVotes > 0) {
 			candidateBlockData.insert({it->first, it->second});
 			it = _data.erase(it);
-			--nTxToRemove;
+			--nbOfVotes;
 		}
 	}
 
