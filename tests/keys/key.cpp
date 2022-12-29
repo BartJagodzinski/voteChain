defbf3edcd36e1145dfaeeb1241e7b58fc518515
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2022-12-29 16:05:58 +0100

    Create key.cpp

---
+++ tests/keys/key.cpp
@@ -0,0 +1,16 @@
+#include "keys.h"
+
+int main() {
+    // If uncompressed key is needed change 33 to 65
+    std::vector<unsigned char> pubKey(33);
+    std::vector<unsigned char> secKey(picosha2::k_digest_size);
+
+    keys::privateKeyFromHash(secKey, keys::hashesFromData("mail", "pass", "name", "surname", 99999));
+
+    // If uncompressed key is needed keys::publicKeyFromSecKey(pubKey, secKey, false)
+    if(keys::publicKeyFromSecKey(pubKey, secKey)) std::cout << "Success" << std::endl;
+
+    std::cout << "Secret Key: "; keys::print_hex(secKey.data(), secKey.size());
+    std::cout << "Public Key: "; keys::print_hex(pubKey.data(), pubKey.size());
+    return EXIT_SUCCESS;
+}
