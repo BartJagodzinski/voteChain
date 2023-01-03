6ce70682de57d50a56035ce00450f7c6d3c2a9b6
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-03 15:43:58 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <tuple>
-#include <map>
+#include <unordered_map>
 #include "merkle.h"
 #include "blockchain.h"
 #include "mempool.h"
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
 	somedata.push_back(std::make_tuple("K", "L"));
 	std::cout << merkle::get_merkle_root_hash(somedata) << std::endl;
 
-	std::map<std::string, std::string> map;
+	std::unordered_map<std::string, std::string> map;
 	map.insert(std::pair<std::string, std::string>("A", "B"));
 	map.insert(std::pair<std::string, std::string>("C", "D"));
 	map.insert(std::pair<std::string, std::string>("E", "F"));
@@ -33,63 +33,6 @@ int main(int argc, char* argv[]) {
 	map.insert(std::pair<std::string, std::string>("I", "J"));
 	map.insert(std::pair<std::string, std::string>("K", "L"));
 	std::cout << merkle::get_merkle_root_hash(map);
-	*/
-
-	// Json test
-	/*
-	// create a map
-	std::map<std::string, std::string> m3{ {"key", "val"}, {"key1", "val1"} };
-
-	// create and print a JSON from the map
-	nlohmann::json j1 = {
-		{"id", 10},
-		{"map", m3}
-	};
-	std::cout << j1 << std::endl;
-
-	// get the map out of JSON
-	std::map<std::string, std::string> m4 = j1["map"];
-	// make sure the roundtrip succeeds
-	assert(m3 == m4);
-	*/
-
-	/*
-	nlohmann::json j = {
-		{"pi", 3.141},
-		{"happy", true},
-		{"name", "Niels"},
-		{"nothing", nullptr},
-		{"answer", {
-			{"everything", 42}
-		}},
-		{"list", {1, 0, 2}},
-		{"object", {
-			{"currency", "USD"},
-			{"value", 42.99}
-		}}
-	};
-
-	std::cout << j << std::endl;
-	std::cout << "###########################" << std::endl;
-	for(auto& i : j)
-		std::cout << i << std::endl;
-	std::cout << "###########################" << std::endl;
-	// even easier with structured bindings (C++17)
-	for (auto& [key, value] : j.items()) {
-		std::cout << key << " : " << value << "\n";
-	}
-	*/
-
-	/*
-	nlohmann::json j2 = {};
-	j2["list"] = { 1,2,3,4 };
-	j2["list1"] = { 1,2,3,4 };
-	j2["pi"] = 3.14159;
-	j2["string"] = "test";
-
-	for (auto& [key, value] : j2.items()) {
-		std::cout << key << " : " << value << "\n";
-	}
 	*/
 
 	// uint256 test
@@ -100,148 +43,74 @@ int main(int argc, char* argv[]) {
 	std::cout << uint256 * two << std::endl;
 	*/
 
-	// mixed tests
-	/*
-	nlohmann::json mixedJson = {};
-	boost::multiprecision::uint256_t bigNumber {"11579208923731619542357098500868790785326998466564056403945758400791312963993"};
-	boost::multiprecision::uint256_t bigNumber2 {"1456734561234562435756745768567845678456743567365487756484673"};
-	boost::multiprecision::uint256_t bigNumber3 = bigNumber * bigNumber2;
-	mixedJson["bigNumber"] = bigNumber.str();
-	mixedJson["bigNumber2"] = bigNumber2.str();
-	mixedJson["bigNumber3"] = bigNumber3.str();
-
-	for (auto& [key, value] : mixedJson.items()) {
-		std::cout << key << " : " << value << "\n";
-	}
-
-	std::string s = mixedJson.dump();
-	std::cout << s << std::endl;
-	s = picosha2::hash256_hex_string(s);
-	std::cout << s << std::endl;
-	*/
-
 	// Blockchain tests
-	/*
-	// 0
-	Blockchain b("voteChain v22.11", 1667732142);
-	b.print();
-	// 1
-	b.addTx("A", "B");
-	b.addTx("C", "D");
-	b.addBlock();
-	system("cls");
-	b.print();
-	// 2
-	b.addTx("E", "F");
-	b.addTx("G", "H");
-	b.addBlock();
-	system("cls");
-	b.print();
-	// 3
-	b.setDifficulty(2);
-	b.addTx("I", "J");
-	b.addTx("K", "L");
-	b.addBlock();
-	system("cls");
-	b.print();
-	// 4
-	b.setDifficulty(4);
-	b.addTx("M", "N");
-	b.addTx("O", "P");
-	b.addTx("Q", "R");
-	b.addTx("S", "T");
-	b.addBlock();
-	system("cls");
-	b.print();
-	// 5
-	b.addTx("U", "V");
-	b.addTx("W", "X");
-	b.addTx("Y", "Z");
-	b.addBlock();
-	system("cls");
-	b.print();
-	*/
-	
+
 	/*
 	Blockchain b("voteChain v22.11", 1667732142);
 	b.printLastBlock();
 
 	for (unsigned int i = 0; i < 100; i++) {
 		for (unsigned int j = 0; j < 10000; j++) {
-			b.addTx(picosha2::hash256_hex_string(std::to_string(j)), picosha2::hash256_hex_string(std::to_string(i)));			
-		}
-		b.addBlock();
-		b.printLastBlock();
-	}
-
-	*/
-
-	// Mempool tests
-	/*
-	// Create mempool and add some votes
-	Mempool m(1667734373);
-	m.addTx("A", "B");
-	m.addTx("C", "D");
-	m.addTx("E", "F");
-	m.addTx("G", "H");
-	m.addTx("I", "J");
-	m.addTx("K", "L");
-	m.addTx("M", "N");
-	m.addTx("O", "P");
-	m.addTx("Q", "R");
-	m.addTx("S", "T");
-	m.addTx("U", "V");
-	m.addTx("W", "X");
-	m.addTx("Y", "Z");
-	m.print();
-
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	// Get 6 votes from mempool to candidate block
-	std::map<std::string, std::string> candidateBlockData;
-	m.getTxs(candidateBlockData, 6);
-	for (auto el : candidateBlockData) std::cout << el.first << ": " << el.second << std::endl;
-
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	// Mempool should not have these 6 votes
-	m.print();
-	*/
-
-	// storage test
-	/*
-	Blockchain b("voteChain v22.11", 1667732142);
-	b.printLastBlock();
-
-	for (unsigned int i = 0; i < 10; i++) {
-		for (unsigned int j = 0; j < 10; j++) {
 			b.addTx(picosha2::hash256_hex_string(std::to_string(j)), picosha2::hash256_hex_string(std::to_string(i)));
 		}
 		b.addBlock();
-		b.printLastBlock();
 	}
-	b.addRemainingTxs();
-	system("cls");
-	
-	Storage* s = b.getStorage();
-	Block* blk = s->readBlockFromJson(10);
 
-	auto data = blk->getData();
+	*/
 
-	assert(merkle::get_merkle_root_hash(data) == blk->getMerkleRoot());
+	// Mempool load from json + listening for votes from clients
+	/*
+	boost::asio::io_context io_context;
 
-	delete blk;
+    boost::asio::ip::tcp::endpoint mempoolListenEndpoint(boost::asio::ip::tcp::v4(), 9876);
+	// Start mempool with deadline 20s from now
+    Mempool mempool(io_context, mempoolListenEndpoint, std::time(nullptr)+20);
+	// Load sample votes from json file, votes can be added by starting client and sending vote from client side
+	if(!mempool.loadVotesFromJson("mempool_load_test.json")) std::cout << "err in loading from json" << std::endl;
 
-	b.printBlockchain();
-	*/		
-	
-	// Node test
-	//*
+	boost::asio::ip::tcp::endpoint nodeListenEndpoint(boost::asio::ip::tcp::v4(), 8888);
+    Node node(io_context, nodeListenEndpoint);
+    boost::asio::ip::tcp::resolver resolver(io_context);
 
-		
-	//*/
+    std::thread t([&io_context](){ io_context.run(); });
+
+	Blockchain b("voteChain v23.1");
+	b.setDifficulty(4);
+	std::unordered_map<std::string, unsigned int> results;
+
+	// Hardcoded lock !mempool.isEmpty() to avoid generating empty blocks
+	while(mempool.isOpen() && !mempool.isEmpty()) {
+		mempool.getVotes(b.getData(), 3);
+		b.addBlock();
+	}
+	b.countVotes(results);
+	std::cout << "Result :" << std::endl;
+	for(auto const &result : results)
+		std::cout << result.first << ": " << result.second <<std::endl;
+
+	Storage* storage = b.getStorage();
+	// Test of func checkIfAddressVoted
+	std::string address = "000c15d0ea8224c9649c9adee30c3030ee769b2b5f6d6594f94d446adf60396b";
+	std::cout <<"Address : " + address + " voted: "  << storage->checkIfAddressVoted(address) << std::endl;
+    node.close();
+	mempool.close();
+	t.join();
+	*/
+
+	// Count votes
+	/*
+
+	Storage storage(50);
+
+	std::unordered_map<std::string, unsigned int> results;
+
+	if(storage.countVotes(results)) {
+		for(auto const &result : results)
+			std::cout << result.first << ": " << result.second <<std::endl;
+	}
+
+	std::cout << storage.checkIfAddressVoted("000c15d0ea8224c9649c9adee30c3030ee769b2b5f6d6594f94d446adf60396b") << std::endl;
+	*/
 
 	return EXIT_SUCCESS;
 }
