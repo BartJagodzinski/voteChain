49a710d6361e2a8726f2fed831dc022fefca756b
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-17 18:58:34 +0100

    Create mempool.cpp

---
+++ tests/checker/mempool/mempool.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include "mempool.h"
+#include "config.h"
+
+int main(int argc, char* argv[]) {
+  try {
+    std::pair<std::string, unsigned short> mempoolIpPort;
+    if(!config::getEndpointsFromJson(mempoolIpPort, "mempool_config.json", "mempool")) { return EXIT_FAILURE; }
+
+    boost::asio::io_context io_context;
+    boost::asio::ip::tcp::endpoint listenEndpoint(boost::asio::ip::address::from_string(mempoolIpPort.first), mempoolIpPort.second);
+
+    Mempool mempool(io_context, listenEndpoint, std::time(nullptr)+600);
+    io_context.run();
+    if(!mempool.loadVotesFromJson("mempool_load_test.json")) std::cout << "err in loading from json" << std::endl;
+
+  } catch (std::exception& e) { std::cerr << "Exception: " << e.what() << "\n"; }
+
+  return EXIT_SUCCESS;
+}
