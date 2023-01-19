c1e935dad128311f7c29f5adac3fcf80dfba20dd
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-19 17:28:35 +0100

    Create vote_sender.h

---
+++ src/vote_sender.h
@@ -0,0 +1,86 @@
+// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <unordered_set>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+#include "message.hpp"
+#include "unordered_set_hash.h"
+#include "ripemd160.h"
+#include "security.h"
+#include "picosha2.h"
+#include "convert.h"
+#include "address.h"
+#include "json.hpp"
+#include "base58.h"
+#include "keys.h"
+#include "config.h"
+
+class VoteSender {
+private:
+  boost::asio::io_context& _io_context;
+  boost::asio::ip::tcp::socket _socket;
+  Message _readMsg;
+  std::vector<char> _buff;
+  std::deque<Message> _writeMsgs;
+
+  void _write() {
+    boost::asio::async_write(_socket, boost::asio::buffer(_writeMsgs.front().data(), _writeMsgs.front().length()),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec) {
+            _writeMsgs.pop_front();
+            if (!_writeMsgs.empty()) _write();
+          }
+          else { std::cerr << ec.message() << std::endl; _socket.close(); }
+        });
+  }
+
+  void _readHeader() {
+    boost::asio::async_read(_socket, boost::asio::buffer(_readMsg.data(), Message::header_length),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec && _readMsg.decode_header()) _readBody();
+          else { std::cerr << ec.message() << std::endl; _socket.close(); }
+        });
+  }
+
+  void _readBody() {
+    boost::asio::async_read(_socket, boost::asio::buffer(_readMsg.body(), _readMsg.body_length()),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec) {
+            std::ofstream whitelistFile("whitelist.json", std::ios::app);
+            whitelistFile << _readMsg.body();
+            whitelistFile.close();
+            _readMsg.clear();
+            _readHeader();
+          }
+          else { std::cerr << ec.message() << std::endl; _socket.close(); }
+        });
+  }
+
+public:
+  VoteSender(boost::asio::io_context& io_context, const boost::asio::ip::tcp::resolver::results_type& endpoints) : _io_context(io_context), _socket(io_context) { connect(endpoints); }
+
+  void connect(const boost::asio::ip::tcp::resolver::results_type& endpoints) { boost::asio::async_connect(_socket, endpoints, [this](boost::system::error_code ec, boost::asio::ip::tcp::endpoint) {
+    if (!ec) _readHeader();
+    else std::cerr << ec.message() << std::endl; });
+  }
+
+  void write(const Message& msg) {
+    boost::asio::post(_io_context,
+        [this, msg]() {
+          bool write_in_progress = !_writeMsgs.empty();
+          _writeMsgs.push_back(msg);
+          if (!write_in_progress) _write();
+        });
+  }
+
+  void close() { boost::asio::post(_io_context, [this]() { _socket.close(); }); }
+};
