8a20f1c45d5f03dffe55caadd4bafd904e885192
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-19 17:23:48 +0100

    Update convert.h

--- src/convert.h
+++ src/convert.h
@@ -2,13 +2,14 @@
 #define CONVERT_H
 #include <sstream>
 #include <vector>
+#include <iomanip>
 
 namespace convert {
 
     std::string uint8_to_hex_string(const uint8_t *data, const size_t size) {
         std::stringstream ss;
         ss << std::hex << std::setfill('0');
-        for (int i = 0; i < size; ++i)
+        for (size_t i = 0; i < size; ++i)
             ss << std::hex << std::setw(2) << static_cast<int>(data[i]);
         return ss.str();
     }
