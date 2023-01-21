fd1c72a9e3a55f9b775600cbcea16ce66c31bc20
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-21 19:06:25 +0100

    Update block.h

--- src/block.h
+++ src/block.h
@@ -10,16 +10,18 @@
 
 class Block {
 private:
-	unsigned int _id;
+	size_t _id;
 	std::time_t _timestamp;
 	std::string _prevHash;
 	std::string _merkleRoot;
 	boost::multiprecision::uint256_t _nonce;
 	std::string _hash;
+	unsigned short _target;
 	std::unordered_map<std::string,std::string> _data;
 public:
-	Block(unsigned int id, std::time_t timestamp, std::string prevHash, std::string merkleRoot, boost::multiprecision::uint256_t nonce, std::string hash, std::unordered_map<std::string, std::string> &data)
-		: _id(id), _timestamp(timestamp), _prevHash(prevHash), _merkleRoot(merkleRoot), _nonce(nonce), _hash(hash), _data(data) { std::cout << "Block C'tor" << std::endl;	}
+	Block(unsigned int id, std::time_t timestamp, std::string prevHash, std::string merkleRoot, boost::multiprecision::uint256_t nonce, std::string hash, unsigned short target, std::unordered_map<std::string, std::string> &data)
+	: _id(id), _timestamp(timestamp), _prevHash(prevHash), _merkleRoot(merkleRoot), _nonce(nonce), _hash(hash), _target(target), _data(data)
+	{ std::cout << "Block C'tor" << std::endl; }
 
 	unsigned int getId() const { return _id; }
 	std::time_t getTimestamp() const { return _timestamp; }
@@ -27,6 +29,7 @@ public:
 	std::string getMerkleRoot() const { return _merkleRoot; }
 	boost::multiprecision::uint256_t getNonce() const { return _nonce; }
 	std::string getHash() const { return _hash; }
+	unsigned short getTarget() const { return _target; }
 	const std::unordered_map<std::string, std::string> &getData() { return _data; }
 
 	void printBlock() {
@@ -35,7 +38,8 @@ public:
 		std::cout << "prevHash   : " << _prevHash << std::endl;
 		std::cout << "merkleRoot : " << _merkleRoot << std::endl;
 		std::cout << "nonce      : " << _nonce << std::endl;
-		std::cout << "hash:      : " << _hash << std::endl;
+		std::cout << "hash       : " << _hash << std::endl;
+		std::cout << "target     : " << _target << std::endl;
 	}
 
 	~Block() { std::cout << "Block D'tor" << std::endl; }
