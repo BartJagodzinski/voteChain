91a8720e49f144ea120ff31d65f8510ec10d0bff
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-21 19:11:35 +0100

    Update mempool_session.h

--- src/mempool_session.h
+++ src/mempool_session.h
@@ -6,43 +6,44 @@
 #include <boost/asio.hpp>
 #include <unordered_map>
 #include <iostream>
+#include <fstream>
 #include <deque>
+#include "mempool_room.h"
 #include "message.hpp"
 #include "voter.h"
-#include "mempool_room.h"
+#include "json.hpp"
 
 class MempoolSession : public Voter, public std::enable_shared_from_this<MempoolSession> {
 private:
-  std::unordered_map<std::string, std::string> &_data;
+  std::unordered_map<std::string, std::string> &_votesToCheck;
   boost::asio::ip::tcp::socket _socket;
   MempoolRoom &_room;
-  Message _read_msg;
+  Message _readMsg;
 
   void _readHeader() {
     auto self(shared_from_this());
-      boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.data(), Message::header_length),
-        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-          if (!ec && _read_msg.decode_header()) _readBody();
-        });
+    boost::asio::async_read(_socket, boost::asio::buffer(_readMsg.data(), Message::header_length),
+      [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+        if (!ec && _readMsg.decode_header()) _readBody();
+        else _room.leave(shared_from_this());
+    });
   }
 
   void _readBody() {
     auto self(shared_from_this());
-      boost::asio::async_read(_socket, boost::asio::buffer(_read_msg.body(), _read_msg.body_length()),
-        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-          if (!ec) {
-            std::string message = _read_msg.body();
-            if(message.size() > 67) {
-              _data.insert({message.substr(0, 34), message.substr(35, 68)});
-              for(auto el : _data) std::cout << el.first <<" : " << el.second << std::endl;
-            }
-            this->~MempoolSession();
-          }
-        });
+    boost::asio::async_read(_socket, boost::asio::buffer(_readMsg.body(), _readMsg.body_length()),
+      [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+        if (!ec) {
+          std::string message = _readMsg.body();
+          if(message.size() > 67 && message.at(0) == '1' && message.at(34) == ':' && message.at(35) == '1') _votesToCheck.insert({message.substr(0, 34), message.substr(35, 68)});
+        }
+        _room.leave(shared_from_this());
+    });
   }
 
 public:
-  MempoolSession(boost::asio::ip::tcp::socket socket, MempoolRoom& room, std::unordered_map<std::string, std::string> &data) : _socket(std::move(socket)), _room(room), _data(data) {  std::cout << "MempoolSession C'tor" << std::endl; }
+  MempoolSession(boost::asio::ip::tcp::socket socket, MempoolRoom &room, std::unordered_map<std::string, std::string> &data)
+  : _socket(std::move(socket)), _room(room), _votesToCheck(data) { std::cout << "MempoolSession C'tor" << std::endl; }
 
   void start() {
     _room.join(shared_from_this());
