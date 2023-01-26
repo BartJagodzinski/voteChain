3e85603c2d852ba473ae966be162ab02771bfec8
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-26 18:25:18 +0100

    Update merkle.h

--- src/merkle.h
+++ src/merkle.h
@@ -3,15 +3,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <tuple>
-#include <map>
 #include <memory>
 #include "picosha2.h"
 #include <boost/multiprecision/cpp_int.hpp>
 
 namespace merkle {
 
-    std::vector<std::string> hash_until_root(std::vector<std::string> const &hashes) {
+    std::vector<std::string> hashUntilRoot(std::vector<std::string> const &hashes) {
         std::vector<std::string> merkleTree;
 
         for (size_t pos = 0; pos + 1 < hashes.size(); pos += 2)
@@ -19,19 +17,22 @@ namespace merkle {
 
         return merkleTree;
     }
-
-    template <class T>
-    std::string get_merkle_root_hash(T const& data) {
-        if (data.size() == 0) return "empty";
+    template <typename T>
+    std::string getMerkleRootHash(T const& votes) {
+        if (votes.size() == 0) return "empty";
+        if (votes.size() == 1) {
+            auto it = votes.begin();
+            return picosha2::double_hash256_hex_string(it->first + it->second);
+        }
         std::vector<std::string> hashes, merkleTree;
         std::string oddHash;
         bool odd = false;
 
-        // Make vec of hashes from <string sender, string receiver> data
-        for (auto& [key, value] : data)
+        // Make vec of hashes from <string sender, string receiver> votes
+        for (auto& [key, value] : votes)
             hashes.push_back(picosha2::double_hash256_hex_string(key + value));
 
-        // If number of hashes is odd, store last hash into variable to vec of hashes make it even
+        // If number of hashes is odd, store last hash into variable to make vec of hashes even
         if (hashes.size() % 2 != 0) {
             odd = true;
             oddHash = hashes.at(hashes.size() - 1);
@@ -40,11 +41,11 @@ namespace merkle {
 
         // Hash until root of even vector<hash>
         do {
-            merkleTree = hash_until_root(hashes);
+            merkleTree = hashUntilRoot(hashes);
             hashes = merkleTree;
         } while (merkleTree.size() > 1);
 
-        // If number of hashes was odd, hash received root of even vec<hash> with stored last odd hash 
+        // If number of hashes was odd, hash received root of even vec<hash> with stored last odd hash
         if (odd) { return picosha2::double_hash256_hex_string(merkleTree.at(0) + oddHash); }
 
         return merkleTree.at(0);
