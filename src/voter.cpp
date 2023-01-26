cadcd9e2b6218b550ce6cc7f4bf9e31a00ac86b8
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-26 18:24:53 +0100

    Update voter.cpp

--- src/voter.cpp
+++ src/voter.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <thread>
+#include <ctime>
 #include <fstream>
 #include <string>
 #include <unordered_set>
@@ -20,11 +21,15 @@ int main(int argc, char* argv[]) {
   try {
     if (argc < 6) { std::cerr << "Usage: ./voter <email> <password> <name> <surname> <id> <receiver>" << std::endl; return EXIT_FAILURE; }
 
+    std::time_t deadline;
+    if(!config::getDeadlineFromJson(deadline, "checker_config.json", "checker")) { return EXIT_FAILURE; }
+    if(std::time(nullptr) > deadline) { std::cerr << "Voting is over. " << std::endl; return EXIT_FAILURE; }
+
     std::string email(argv[1]); std::string password(argv[2]); std::string name(argv[3]); std::string surname(argv[4]); std::string id(argv[5]); std::string receiver(argv[6]);
     boost::to_lower(email); boost::to_lower(name); boost::to_lower(surname); boost::to_lower(receiver);
 
     std::pair<std::string, unsigned short> checkerIpPort;
-    if(!config::getEndpointsFromJson(checkerIpPort, email+".json", "checker")) { return EXIT_FAILURE; }
+    if(!config::getEndpointsFromJson(checkerIpPort, "checker_config.json", "checker")) { return EXIT_FAILURE; }
 
     boost::asio::io_context io_context1;
     boost::asio::ip::tcp::resolver resolver1(io_context1);
@@ -34,14 +39,14 @@ int main(int argc, char* argv[]) {
 
     sleep(1);
 
-    voteSenderToChecker.close();
-    thrd1.join();
-
     std::unordered_set<std::tuple<std::string, std::string, std::string, std::string, std::string>, UnorderedSetHashTuple<std::string>> whitelist;
-    if(!config::getWhitelistFromJson(whitelist)) { return EXIT_FAILURE; }
+    std::pair<std::string, unsigned short> mempoolIpPort;
+
+    std::vector<char> buff = voteSenderToChecker.getBuff();
+    if(!config::getWhitelistAndMempoolEndpoint(buff, whitelist, mempoolIpPort)) { return EXIT_FAILURE; }
     bool check = false;
     for(auto const &voter : whitelist) {
-      if(std::get<0>(voter) == email &&
+      if(std::get<0>(voter) == picosha2::hash256_hex_string(email) &&
         std::get<1>(voter) == picosha2::hash256_hex_string(password) &&
         std::get<2>(voter) == picosha2::hash256_hex_string(name) &&
         std::get<3>(voter) == picosha2::hash256_hex_string(surname) &&
@@ -50,12 +55,11 @@ int main(int argc, char* argv[]) {
           check = true;
         }
     }
-    security::deleteWhitelistFile();
+
+    voteSenderToChecker.close();
+    thrd1.join();
 
     if(!check) { std::cerr << "Wrong email / password / name / surname / id, try again." << std::endl; return EXIT_FAILURE; }
-
-    std::pair<std::string, unsigned short> nodeIpPort;
-    if(!config::getEndpointsFromJson(nodeIpPort, "node_endpoint.json", "node")) return EXIT_FAILURE;
 
     std::string address;
     if(!address::getAddress(address, email, password, name, surname, id)) return EXIT_FAILURE;
@@ -63,12 +67,12 @@ int main(int argc, char* argv[]) {
     std::unordered_set<std::pair<std::string, std::string>, UnorderedSetHashPair<std::string, std::string>> candidates;
     if(!config::getCandidatesFromJson(candidates)) return EXIT_FAILURE;
 
-    std::string voteSting;
+    std::string voteStr;
     std::string receiverAddr;
     bool isCandidateOnList = false;
     for(auto const &candidate : candidates) {
       if(candidate.first == receiver) {
-        voteSting = address + ":" + candidate.second;
+        voteStr = address + ":" + candidate.second;
         receiverAddr = candidate.second;
         isCandidateOnList = true;
       }
@@ -76,38 +80,31 @@ int main(int argc, char* argv[]) {
 
     if(!isCandidateOnList) { std::cerr << "Candidate is not on list, check again your input" << std::endl; return EXIT_FAILURE; }
 
-    char vote[95]="";
-    std::strcat(vote, voteSting.c_str());
     std::time_t timestamp = std::time(nullptr);
-    std::string timestampStr = ":"+std::to_string(timestamp);
-    std::strcat(vote, timestampStr.c_str());
-
 		boost::multiprecision::uint256_t nonce = 0;
-		std::string toHash = picosha2::hash256_hex_string(address+receiverAddr+std::to_string(timestamp));
+		std::string toHash = address+receiverAddr+std::to_string(timestamp);
 		std::string hash;
 		do {
 			++nonce;
 			hash = toHash + nonce.str();
 			hash = picosha2::hash256_hex_string(hash);
     } while (hash.substr(0, 5) != "00000");
+    if(std::time(nullptr) > deadline) { std::cerr << "Voting is over. " << std::endl; return EXIT_FAILURE; }
 
-    std::string nonceStr = ":"+nonce.str();
-    std::strcat(vote, nonceStr.c_str());
-
+    voteStr = voteStr+":"+std::to_string(timestamp)+":"+nonce.str()+";";
+    char vote[voteStr.size()]="";
+    std::strcat(vote, voteStr.c_str());
     Message msg;
-    msg.body_length(std::strlen(vote));
+    msg.body_length(voteStr.size());
     std::memcpy(msg.body(), vote, msg.body_length());
     msg.encode_header();
 
     boost::asio::io_context io_context2;
     boost::asio::ip::tcp::resolver resolver2(io_context2);
-    auto nodeConnectEndpoint = resolver2.resolve(nodeIpPort.first, std::to_string(nodeIpPort.second));
-    VoteSender voteSenderToNode(io_context2, nodeConnectEndpoint);
+    auto mempoolConnectEndpoint = resolver2.resolve(mempoolIpPort.first, std::to_string(mempoolIpPort.second));
+    VoteSender voteSenderToMempool(io_context2, mempoolConnectEndpoint);
     std::thread thrd2([&io_context2](){ io_context2.run(); });
-    voteSenderToNode.write(msg);
-    sleep(1);
-
-    voteSenderToNode.close();
+    voteSenderToMempool.write(msg);
     thrd2.join();
   } catch (std::exception& e) { std::cerr << "Exception: " << e.what() << std::endl; }
 
