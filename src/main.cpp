e50fd612010e533d6efb62c1faf8cf790491a9f9
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-01-26 18:27:01 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <tuple>
+#include <ctime>
 #include <boost/multiprecision/cpp_int.hpp>
 #include "blockchain.h"
 #include "picosha2.h"
@@ -24,43 +24,56 @@ int main(int argc, char* argv[]) {
 
 		boost::asio::io_context ioContextMempool;
 		boost::asio::ip::tcp::endpoint mempoolListenEndpoint(boost::asio::ip::address::from_string(mempoolIpPort.first), mempoolIpPort.second);
-
-		Mempool mempool(ioContextMempool, mempoolListenEndpoint, std::time(nullptr)+30);
+		std::time_t deadline;
+		if(!config::getDeadlineFromJson(deadline, "mempool_config.json", "mempool")) { return EXIT_FAILURE; }
+		Mempool mempool(ioContextMempool, mempoolListenEndpoint, deadline);
 		std::thread mempoolThrd([&ioContextMempool](){ ioContextMempool.run(); });
 
-		if(!mempool.loadVotesFromJson("mempool_load_test.json")) std::cerr << "Error in loading votes from json." << std::endl;
+		//if(!mempool.loadVotesFromJson("mempool_load_test.json")) std::cerr << "Error in loading votes from json." << std::endl;
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
-
+	// Checker
+	/*
 		std::pair<std::string, unsigned short> checkerIpPort;
 		if(!config::getEndpointsFromJson(checkerIpPort, "checker_config.json", "checker")) { return EXIT_FAILURE; }
 		boost::asio::io_context ioContextChecker;
 		boost::asio::ip::tcp::endpoint checkerListenEndpoint(boost::asio::ip::address::from_string(checkerIpPort.first), checkerIpPort.second);
 
-		Checker checker(ioContextChecker, checkerListenEndpoint);
+		Checker checker(ioContextChecker, checkerListenEndpoint, deadline);
 		std::thread checkerThrd([&ioContextChecker](){ ioContextChecker.run(); });
-
-
+	*/
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 
-		Blockchain blockchain("voteChain v23.1");
-		blockchain.setDifficulty(4);
+		std::string blockchainName;
+		if(!config::getBlockchainNameFromJson(blockchainName, "mempool_config.json", "mempool")) { return EXIT_FAILURE; }
+		Blockchain blockchain(blockchainName);
+		unsigned short difficulty;
+		if(!config::getDifficultyFromJson(difficulty, "mempool_config.json", "mempool")) { return EXIT_FAILURE; }
+		blockchain.setDifficulty(difficulty);
 
 		while(mempool.isOpen() || !mempool.isEmpty()) {
-			mempool.getVotes(blockchain.getData(), 10, blockchain.getLenght());
+			mempool.getVotes(blockchain.getVotes(), 10, blockchain.getLenght());
 			blockchain.addBlock();
 		}
 
+		if(!mempool.votesToCheckEmpty()) {
+			mempool.getVotes(blockchain.getVotes(), 10, blockchain.getLenght());
+			mempool.addLastVote(blockchain.getVotes(), blockchain.getLenght());
+			blockchain.addBlock();
+		}
+
+		mempool.close();
+    	mempoolThrd.join();
+		//checker.close();
+    	//checkerThrd.join();
+		if(!blockchain.verifyBlockchain()) { std::cerr << "Blockchain is invalid." <<std::endl; return EXIT_FAILURE; }
+
 		std::unordered_map<std::string, unsigned int> results;
-		blockchain.countVotes(results);
+		storage::countVotes(results, blockchain.getLenght());
 		std::cout << "Result :" << std::endl;
 		for(auto const &result : results)
 			std::cout << result.first << ": " << result.second <<std::endl;
 
-		mempool.close();
-    	mempoolThrd.join();
-		checker.close();
-    	checkerThrd.join();
   	} catch (std::exception& e) { std::cerr << "Exception: " << e.what() << std::endl; return EXIT_FAILURE; }
 	return EXIT_SUCCESS;
 //*/
