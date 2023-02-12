863178de7b4d6b7311e82842b6f8d8dfdef236e9
BartJagodzinski <72560501+BartJagodzinski@users.noreply.github.com>
2023-02-12 16:13:26 +0100

    Update send_mail.py

--- tests/email/send_mail.py
+++ tests/email/send_mail.py
@@ -14,13 +14,16 @@ jsonData = []
 with open('whitelist.json') as whitelistFile:
     jsonData = json.load(whitelistFile)
 
+checkerConfigFile = open('checker_config.json')
+checkerConfig = json.load(checkerConfigFile)
+
 for voter in jsonData['voters']:
     email = MIMEMultipart()
     email['From'] = senderMail
     email['Subject'] = 'Your configuration file for voteChain'
     email['To'] = voter["email"]
     email.attach(MIMEText("File checker_config.json is in attachments. Copy it to folder where your client program is executed.", 'plain'))
-    attachment = MIMEText(json.dumps(voter))
+    attachment = MIMEText(json.dumps(checkerConfig))
     attachment.add_header('Content-Disposition', 'attachment', filename="checker_config.json")
     email.attach(attachment)
     smtp.sendmail(senderMail, voter["email"], email.as_string())
